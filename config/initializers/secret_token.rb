2535866d998655bb821dff17ca84375ddb00cee5
thangnv27 <ngothangit@gmail.com>
2012-08-13 10:33:12 +0700

    First Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ProjectGit::Application.config.secret_token = '2c5e43927930f78c7f269f7caa1794c14fca64c01f534aaab7a5a777a67bd8d5405f5c0893254299a7379257b58c10f802fdc0fda6f38166c2552b4ba4cecea7'
